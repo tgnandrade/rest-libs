0a677befdc2828c6068c85a4d5f4798faff9e0f6
Tiago Andrade <tiago.andrade-ext@viavarejo.com.br>
2021-09-27 14:37:50 -0300

    Criando repositório

---
+++ RestLibs/ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace RestLibs
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepViewModel> GetAddressAsync(string cep);
+    }
+}
---
+++ RestLibs/Program.cs
@@ -0,0 +1,38 @@
+﻿using Flurl;
+using Flurl.Http;
+using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace RestLibs
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = RestService.For<ICepApiService>("https://viacep.com.br");
+            string meuCep = "81940320";
+            Console.WriteLine($"Consultando dados do CEP: {meuCep}");
+
+            var address = await client.GetAddressAsync(meuCep);
+            Console.Write($"\nLogradouro: {address.Logradouro}\nBairro: {address.Bairro}\nEstado: {address.Uf}\nCódigo Ibge: {address.Ibge}");
+            Console.WriteLine(Environment.NewLine);
+
+            var url = "https://pokeapi.co/api/v2/pokemon";
+
+            var result = await url
+                .SetQueryParams(new { limit = 50 })
+                .GetJsonAsync<NamedAPIResourceList>();
+
+            Console.WriteLine($"Count: {result.Count}");
+
+            foreach (var item in result.Results)
+            {
+                Console.WriteLine($"Name: {item.Name}\nUrl: { item.Url}");
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
